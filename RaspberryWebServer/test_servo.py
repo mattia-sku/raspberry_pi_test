d45d31aef295509c8cf302bf0aa35317cd8191aa
Dave98-dev <71784042+Dave98-dev@users.noreply.github.com>
2021-09-01 21:15:21 +0200

    test

--- RaspberryWebServer/test_servo.py
+++ RaspberryWebServer/test_servo.py
@@ -4,7 +4,6 @@ import time
 
 # Set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
-
 # Set pin 11 as an output, and set servo1 as pin 11 as PWM
 GPIO.setup(11,GPIO.OUT)
 servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
@@ -21,7 +20,7 @@ print ("Rotating 180 degrees in 10 steps")
 duty = 2
 
 # Loop for duty values from 2 to 12 (0 to 180 degrees)
-while duty <= 12:
+while duty <= 24:
     servo1.ChangeDutyCycle(duty)
     time.sleep(1)
     duty = duty + 1

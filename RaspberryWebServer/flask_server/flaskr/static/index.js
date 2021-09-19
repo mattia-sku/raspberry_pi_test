9205db74dcaffb5955b3c04d00bfebcf0f9ee044
Dave98-dev <71784042+Dave98-dev@users.noreply.github.com>
2021-09-19 18:56:19 +0200

    Update index.js

--- RaspberryWebServer/flask_server/flaskr/static/index.js
+++ RaspberryWebServer/flask_server/flaskr/static/index.js
@@ -1,5 +1,5 @@
 async function buttonClick () {
-  let value = document.getElementById('quantity').innerText;
+  let value = document.getElementById('quantity').value;
   fetch(`/test_rasp/${value}`).then(res => res.json()).then(_ => {
     window.location.href = '/review';
   });
